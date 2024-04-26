37113ac1aa89289e0e8e974dfd1b1a4b43f9c69b
ChoiYunYu <e1139545@u.nus.edu>
2024-04-26 20:01:07 +0900

    round 2

---
+++ scatter/scattertoken.R
@@ -0,0 +1,4 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='tayyibahshah',
+                          token='919E1000C1AB381EE562AC6C57D4CBE0',
+                          secret='TsCDKbZWENA221iteivWpCxB+zbquCcMokJ7sDlk')
